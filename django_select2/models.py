67d93642c9af2150fef7669733def17c245e76d2
Jerin Peter George <jerinpetergeorge@gmail.com>
2020-02-11 17:47:32 +0530

    autocomplete and pagination ok

--- django_select2/models.py
+++ django_select2/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Musician(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Album(models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} : {self.artist}'
--- root/urls.py
+++ root/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django_select2.views import select2
+from django_select2.views import select2, AlbumAutoCompleteView
 
 urlpatterns = [
     path('', select2, name='select2-test'),
+    path('api/v1/albums/', AlbumAutoCompleteView.as_view(), name='albums'),
     path('api/v1/', include(('charts.apis.urls', 'charts'), namespace='chart-api')),
     path('chart/', include(('charts.urls', 'charts'), namespace='chart')),
     path('admin/', admin.site.urls),
